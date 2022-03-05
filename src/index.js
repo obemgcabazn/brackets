fe95afba1faa312f2f0f7b28ceb22c02188c4c45
Electropompa <admin@zilmet.ru>
2022-03-05 15:22:51 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) {
+    return false;
+  } else {
+    let line = str;
+    let index = 0;
+    const mask = bracketsConfig.map((elm) => elm.join(''));
+    recicle();
+
+    function recicle() {
+      for (let val of mask) {
+        for (let i = 0; i < str.length; i += 2) {
+          if (line.includes(val)) {
+            line = line.replace(val, '');
+          } else {
+            break;
+          };
+        };
+      };
+      index++;
+      if (index <= (mask.length) && line.length > 0) {
+        recicle();
+      };
+    };
+
+    return line.length === 0;
+  };
 }
